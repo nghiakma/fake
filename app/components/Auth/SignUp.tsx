50b41d4d34d1224057fe4715d5be64b03c188b1f
nghiakma <lam47788@gmail.com>
2024-09-11 04:55:26 +0700

    first commit

---
+++ app/components/Auth/SignUp.tsx
@@ -0,0 +1,146 @@
+"use client";
+import React, { FC, useEffect, useState } from "react";
+import { useFormik } from "formik";
+import * as Yup from "yup";
+import { AiOutlineEye, AiOutlineEyeInvisible } from "react-icons/ai";
+import { styles } from "../../../app/styles/style";
+import { useRegisterMutation } from "@/redux/features/auth/authApi";
+import { toast } from "react-hot-toast";
+
+type Props = {
+  setRoute: (route: string) => void;
+};
+
+const schema = Yup.object().shape({
+  name: Yup.string().required("Vui lòng nhập tên của bạn!"),
+  email: Yup.string()
+    .email("Email không hợp lệ!")
+    .required("Vui lòng nhập email của bạn!"),
+  password: Yup.string().required("Vui lòng nhập mật khẩu của bạn!").min(6),
+});
+
+const Signup: FC<Props> = ({ setRoute }) => {
+  const [show, setShow] = useState(false);
+  const [register, { data, error, isSuccess }] = useRegisterMutation();
+
+  useEffect(() => {
+    if (isSuccess) {
+      const message = data?.message || "Đăng ký thành công";
+      toast.success(message);
+      setRoute("Xác minh");
+    }
+    if (error) {
+      if ("data" in error) {
+        const errorData = error as any;
+        toast.error(errorData.data.message);
+      }
+    }
+  }, [isSuccess, error]);
+
+  const formik = useFormik({
+    initialValues: { name: "", email: "", password: "" },
+    validationSchema: schema,
+    onSubmit: async ({ name, email, password }) => {
+      const data = {
+        name,
+        email,
+        password,
+      };
+      await register(data);
+    },
+  });
+
+  const { errors, touched, values, handleChange, handleSubmit } = formik;
+
+  return (
+    <div className="w-full">
+      <h1 className={`${styles.title}`}>Tham gia</h1>
+      <form onSubmit={handleSubmit}>
+        <div className="mb-3">
+          <label className={`${styles.label}`} htmlFor="email">
+            Nhập tên của bạn
+          </label>
+          <input
+            type="text"
+            name=""
+            value={values.name}
+            onChange={handleChange}
+            id="name"
+            placeholder="johndoe"
+            className={`${errors.name && touched.name && "border-red-500"} ${
+              styles.input
+            }`}
+          />
+          {errors.name && touched.name && (
+            <span className="text-red-500 pt-2 block">{errors.name}</span>
+          )}
+        </div>
+        <label className={`${styles.label}`} htmlFor="email">
+           Nhập email của bạn
+        </label>
+        <input
+          type="email"
+          name=""
+          value={values.email}
+          onChange={handleChange}
+          id="email"
+          placeholder="loginmail@gmail.com"
+          className={`${errors.email && touched.email && "border-red-500"} ${
+            styles.input
+          }`}
+        />
+        {errors.email && touched.email && (
+          <span className="text-red-500 pt-2 block">{errors.email}</span>
+        )}
+        <div className="w-full mt-5 relative mb-1">
+          <label className={`${styles.label}`} htmlFor="email">
+            Nhập password của bạn
+          </label>
+          <input
+            type={!show ? "password" : "text"}
+            name="password"
+            value={values.password}
+            onChange={handleChange}
+            id="password"
+            placeholder="password!@%"
+            className={`${
+              errors.password && touched.password && "border-red-500"
+            } ${styles.input}`}
+          />
+          {!show ? (
+            <AiOutlineEyeInvisible
+              className="absolute bottom-3 right-2 z-1 cursor-pointer"
+              size={20}
+              onClick={() => setShow(true)}
+            />
+          ) : (
+            <AiOutlineEye
+              className="absolute bottom-3 right-2 z-1 cursor-pointer"
+              size={20}
+              onClick={() => setShow(false)}
+            />
+          )}
+        </div>
+        {errors.password && touched.password && (
+          <span className="text-red-500 pt-2 block">{errors.password}</span>
+        )}
+        <div className="w-full mt-5">
+          <input type="submit" value="Sign Up" className={`${styles.button}`} />
+        </div>
+        <br />
+      </form>
+      <br />
+      <h5>
+        Quay lại{" "}
+        <span
+          className="text-blue-500 cursor-pointer"
+          onClick={() => setRoute("Login")}
+        >
+          Đăng nhập!
+        </span>
+      </h5>
+    </div>
+  );
+};
+
+export default Signup;
